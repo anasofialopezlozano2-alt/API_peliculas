422e922299b01677d87a94b54e15ee38531f79f2
Ana Sofia <anasofialopezlozano2@gamil.com>
2026-03-12 12:26:40 -0500

    Carousel integration

--- app.js
+++ app.js
@@ -4,58 +4,94 @@ const btnAnterior = document.getElementById("btnAnterior");
 const btnSiguiente = document.getElementById("btnSiguiente");
 
 btnSiguiente.addEventListener("click", () => {
-    pagina += 1;
-    cargarPeliculas();
+pagina += 1;
+cargarPeliculas();
 });
 
 btnAnterior.addEventListener("click", () => {
-    if(pagina > 1){
-        pagina -= 1;
-        cargarPeliculas();
-    }
+if(pagina > 1){
+pagina -= 1;
+cargarPeliculas();
+}
 });
 
-const cargarPeliculas = async() => {
+const cargarCarrusel = async () => {
 
-    try{
+const respuesta = await fetch(
+"https://api.themoviedb.org/3/trending/movie/week?api_key=e6e4c450fbf961c15520e8eb20be3813&language=es"
+);
 
-        const respuesta = await fetch(
-        `https://api.themoviedb.org/3/movie/popular?api_key=e6e4c450fbf961c15520e8eb20be3813&language=es-COL&page=${pagina}`
-        );
+const datos = await respuesta.json();
 
-        if (respuesta.status === 200){
+let peliculas = "";
 
-            const datos = await respuesta.json();
+datos.results.forEach(pelicula => {
 
-            let peliculas = "";
+peliculas += `
+<a href="pelicula.html?id=${pelicula.id}">
 
-            datos.results.forEach(pelicula => {
+<img class="poster-carrusel"
+src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
 
-                peliculas += `
-                <a href="pelicula.html?id=${pelicula.id}">
-                    <div class="pelicula">
-                        <img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
-                        <h3 class="titulo">${pelicula.title}</h3>
-                    </div>
-                </a>
-                `;
+</a>
+`;
 
-            });
+});
 
-            document.getElementById("contenedor").innerHTML = peliculas;
-
-        } else if(respuesta.status === 401){
-            console.log("Pusiste la llave mal");
-        } else if(respuesta.status === 404){
-            console.log("La pelicula que buscas no existe");
-        } else{
-            console.log("Error desconocido");
-        }
-
-    } catch(error){
-        console.log(error);
-    }
+document.getElementById("carrusel").innerHTML = peliculas;
 
 }
 
+const cargarPeliculas = async() => {
+
+try{
+
+const respuesta = await fetch(
+`https://api.themoviedb.org/3/movie/popular?api_key=e6e4c450fbf961c15520e8eb20be3813&language=es&page=${pagina}`
+);
+
+const datos = await respuesta.json();
+
+let peliculas = "";
+
+datos.results.forEach(pelicula => {
+
+peliculas += `
+<a href="pelicula.html?id=${pelicula.id}" class="link-pelicula">
+
+<div class="pelicula">
+
+<img class="poster"
+src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
+
+<h3 class="titulo">${pelicula.title}</h3>
+
+<p class="rating"> Puntuación: ${pelicula.vote_average}</p>
+
+</div>
+
+</a>
+`;
+
+});
+
+document.getElementById("contenedor").innerHTML = peliculas;
+
+}catch(error){
+console.log(error);
+}
+
+}
+
+const carrusel = document.getElementById("carrusel");
+
+document.getElementById("btnDerecha").addEventListener("click",()=>{
+carrusel.scrollLeft += 400;
+});
+
+document.getElementById("btnIzquierda").addEventListener("click",()=>{
+carrusel.scrollLeft -= 400;
+});
+
+cargarCarrusel();
 cargarPeliculas();
--- detalle.js
+++ detalle.js
@@ -3,39 +3,66 @@ const id = parametros.get("id");
 
 const cargarPelicula = async () => {
 
-    const respuesta = await fetch(
-    `https://api.themoviedb.org/3/movie/${id}?api_key=e6e4c450fbf961c15520e8eb20be3813&language=es`
-    );
+const respuesta = await fetch(
+`https://api.themoviedb.org/3/movie/${id}?api_key=e6e4c450fbf961c15520e8eb20be3813&language=es`
+);
 
-    const datos = await respuesta.json();
+const datos = await respuesta.json();
 
-    const pelicula = `
-    <div class="hero" style="background-image:url(https://image.tmdb.org/t/p/original/${datos.backdrop_path})">
+const respuestaVideos = await fetch(
+`https://api.themoviedb.org/3/movie/${id}/videos?api_key=e6e4c450fbf961c15520e8eb20be3813`
+);
 
-        <div class="tarjeta">
+const datosVideos = await respuestaVideos.json();
 
-            <img class="poster" src="https://image.tmdb.org/t/p/w500/${datos.poster_path}">
+const trailer = datosVideos.results.find(video => video.type === "Trailer");
 
-            <div class="info">
+const pelicula = `
+<div class="hero"
+style="background-image:url(https://image.tmdb.org/t/p/original/${datos.backdrop_path})">
 
-                <h1>${datos.title}</h1>
+<div class="tarjeta">
 
-                <p class="rating">⭐ ${datos.vote_average}</p>
+<img class="poster-detalle"
+src="https://image.tmdb.org/t/p/w500/${datos.poster_path}">
 
-                <p class="fecha">📅 ${datos.release_date}</p>
+<div class="info">
 
-                <p class="descripcion">${datos.overview}</p>
+<h1>${datos.title}</h1>
 
-                <a class="volver" href="index.html">⬅ Volver</a>
+<p>✰ Puntuación: ${datos.vote_average}</p>
+<p>📅︎ Fecha de estreno: ${datos.release_date}</p>
+<p>⏱︎ Duración: ${datos.runtime} min</p>
 
-            </div>
+<p>🏷︎ Genero: ${datos.genres.map(g => g.name).join(", ")}</p>
 
-        </div>
+<p>🏢︎ Produtora: ${datos.production_companies.map(p => p.name).join(", ")}</p>
 
-    </div>
-    `;
+<p>✎ Descripción: ${datos.overview}</p>
 
-    document.getElementById("detalle").innerHTML = pelicula;
+<div class="botones">
+
+<a href="index.html" class="volver">⬅ Volver</a>
+
+${trailer ? `
+<a class="btn-trailer"
+href="https://www.youtube.com/watch?v=${trailer.key}"
+target="_blank">
+📽︎ Ver Trailer
+</a>
+` : ""}
+
+</div>
+
+</div>
+
+</div>
+
+</div>
+`;
+
+document.getElementById("detalle").innerHTML = pelicula;
+
 }
 
 cargarPelicula();
